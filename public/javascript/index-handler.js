1ec28451ccb81d24bd6a954e60d601703ff97fc9
Tim <tim.bathgate@gmail.com>
2016-05-24 19:02:30 +1200

    Add users that aren't logged in cannot access listings

--- public/javascript/index-handler.js
+++ public/javascript/index-handler.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
 
     if (!localStorage.getItem("loggedInAs")){
         
-        $("#content").remove();
+        $(".page-header").nextAll().remove();
+        $("#advancedSearch").remove();
         $("#newListings").remove();
+        $("#administrate").remove();
+        $(".bs-docs-section").append("<h1>You need to log in...</h1>");
     }
 
 });
