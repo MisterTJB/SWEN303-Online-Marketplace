7187e413b9ab0a06c69ad36563f1479e710c42f9
Tim <tim.bathgate@gmail.com>
2016-05-25 14:53:24 +1200

    Add altering PRODUCTS_IN_QUEUE removes products from new queue, if appropriate

--- routes/site-parameters-endpoint.js
+++ routes/site-parameters-endpoint.js
@@ -134,6 +134,7 @@ router.post('/queue-length/:newValue', function(req, res, next) {
     var newProductQueueLength = parseInt(req.params.newValue);
     console.log("ATTEMPTING TO ALTER NEW QUEUE LENGTH");
 
+
     pg.connect(global.databaseURI, function(err, client, done) {
         if(err){
             console.error('Could not connect to the database');
@@ -141,19 +142,42 @@ router.post('/queue-length/:newValue', function(req, res, next) {
             return;
         }
 
-        var QUERY = "UPDATE site_parameters SET value=%NEW% WHERE parameter='PRODUCTS_IN_QUEUE';".replace("%NEW%", newProductQueueLength);
+        var QUERY = "UPDATE site_parameters SET value=%NEW% WHERE parameter='PRODUCTS_IN_QUEUE'".replace("%NEW%", newProductQueueLength);
 
         console.log(QUERY);
-        client.query(QUERY, function(error, result){
+        client.query(QUERY, function(error, result) {
             done();
-            if (error){
+            if (error) {
                 console.log(error);
             }
 
+            var QUERY = "SELECT sid FROM stock WHERE status='pending' ORDER BY sid ASC;";
+
+            console.log(QUERY);
+            client.query(QUERY, function (error, result) {
+                done();
+                if (error) {
+                    console.log(error);
+                }
+                console.log("The rows to remove: " + result.rowCount);
+                if (result.rowCount > newProductQueueLength) {
+                    var toRemove = result.rows.slice(0, result.rowCount - newProductQueueLength);
+                    toRemove = toRemove.map(function(el){return el.sid});
+                    var QUERY = "UPDATE stock SET status='unsuccessful' WHERE sid IN (%TO_REMOVE%);".replace("%TO_REMOVE%", toRemove);
+                    console.log(QUERY);
+                    client.query(QUERY, function (error, result) {
+                        done();
+                        if (error) {
+                            console.log(error);
+                        }
+                    });
+                }
+
+            });
         });
-
-
     });
+
+
 });
 
 module.exports = router;
